32a0c437688b1047235aca7816a17e4476f680a2
hansrajathole <hansrajathole1@gmail.com>
2026-01-20 17:24:06 +0530

    if else

---
+++ Lec3/OddEven.java
@@ -0,0 +1,14 @@
+package Lec3;
+
+public class OddEven {
+    public static void main(String[] args) {
+        
+        int num =5;
+
+        if(num%2 == 0){
+            System.out.println("The number is Even");
+        }else {
+            System.out.println("Odd");
+        }
+    }
+}
---
+++ Lec3/Sum.java
@@ -0,0 +1,13 @@
+package Lec3;
+
+public class Sum {
+    public static void main(String[] args) {
+        
+        int a = 40;
+        int b = 30;
+
+        int sum = a+b;
+
+        System.out.println("sum is : "+sum);
+    }
+}
