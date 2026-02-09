f04690a385bcea594fc150dc06e24ad485c70196
hansrajathole <hansrajathole1@gmail.com>
2026-02-09 21:16:02 +0530

    Functions / methods

---
+++ Lec14/Fun_Demo.java
@@ -0,0 +1,20 @@
+package Lec14;
+
+public class Fun_Demo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        int a = 10;
+        int b = 50;
+        Add(b);
+        System.out.println("Bye");
+
+    }
+
+    public static void Add(int b){
+        int a = 10;
+       
+        System.out.println(a+b);
+
+    }
+}
